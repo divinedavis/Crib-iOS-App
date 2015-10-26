40bd9409eb0d031676040152053729f7c8e13f95
divinedavis <divinejdavis@gmail.com>
2015-10-26 14:09:35 -0400

    Ran into a bug

--- Crib/ParseStarterProject/UserTableViewController.swift
+++ Crib/ParseStarterProject/UserTableViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 
 class UserTableViewController: UITableViewController {
 
+    var users = [String]()
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
@@ -18,7 +20,20 @@ class UserTableViewController: UITableViewController {
         //Printing the currentUser name
         print(PFUser.currentUser()!)
         
+        var query = PFUser.query()
         
+        query?.findObjectsInBackgroundWithBlock({ (objects : [AnyObject]!, error : NSError!) -> Void in
+            
+            self.users.removeAll(keepCapacity : true)
+            
+            for object in objects {
+                
+                var user : PFUser = (object as? PFUser)!
+                
+                self.users.append(user.username!)
+                
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,70 +41,68 @@ class UserTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
 
-//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
+        return 1
+    }
 
-//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return 0
-//    }
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+    }
 
-    /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
+        
+        let cell : UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+        
+        cell.textLabel?.text = users[indexPath.row]
 
         return cell
     }
-    */
+
 
     /*
-    // Override to support conditional editing of the table view.
+     Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
+         Return false if you do not want the specified item to be editable.
         return true
     }
     */
 
     /*
-    // Override to support editing the table view.
+     Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
-            // Delete the row from the data source
+             Delete the row from the data source
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+             Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
     */
 
     /*
-    // Override to support rearranging the table view.
+     Override to support rearranging the table view.
     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
 
     }
     */
 
     /*
-    // Override to support conditional rearranging of the table view.
+     Override to support conditional rearranging of the table view.
     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
+         Return false if you do not want the item to be re-orderable.
         return true
     }
     */
 
     /*
-    // MARK: - Navigation
+     MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+     In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+         Get the new view controller using segue.destinationViewController.
+         Pass the selected object to the new view controller.
     }
     */
 
